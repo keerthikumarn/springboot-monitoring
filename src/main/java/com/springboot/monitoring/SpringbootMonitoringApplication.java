9e9e9c4af3b56d9faf090956fdc5fa5345b79c08
n.keerthikumar85@gmail.com <n.keerthikumar85@gmail.com>
2024-11-24 13:07:18 +0530

    implemented the monitoring mechanism with grafana and prometheus

---
+++ src/main/java/com/springboot/monitoring/SpringbootMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/monitoring/springboot_monitoring/SpringbootMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.monitoring.springboot_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
